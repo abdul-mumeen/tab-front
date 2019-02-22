60bcda5a142ad9d5e251680580de71b49c9abf5d
phabbs <abdulmumeen.olasode@andela.com>
2019-02-22 16:48:11 +0100

    update user dashboard'

--- src/app/shared/pages/user/user.component.ts
+++ src/app/shared/pages/user/user.component.ts
@@ -3,19 +3,40 @@ import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 
+import { TableSelect } from '../../../../models/models';
+
 @Component({
     templateUrl: 'user.component.html',
     styleUrls: ['user.component.scss'],
 })
 export class UserComponent implements OnInit, OnDestroy {
     loading: boolean = false;
+    tables: TableSelect[] = [];
+    disableSelect: boolean = true;
+    selectPlaceholder: string = 'No table available';
+
     constructor(
         private loc: Location,
         private authService: AuthService,
         private router: Router,
     ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.tables = [
+            {
+                displayName: 'chicken salad',
+                value: 'chicken salad',
+            },
+            {
+                displayName: 'pension',
+                value: 'pension',
+            },
+        ];
+        this.disableSelect = false;
+        this.selectPlaceholder = !!this.tables
+            ? 'Select table'
+            : 'No table available';
+    }
     ngOnDestroy() {}
 
     async goHome() {
