a049bc72ca0b4980c2a374cb37c8e4a98c66f1eb
phabbs <abdulmumeen.olasode@andela.com>
2019-03-18 23:38:16 +0100

    add tableau refresh

--- src/app/shared/pages/table/add.component.ts
+++ src/app/shared/pages/table/add.component.ts
@@ -8,6 +8,8 @@ import { MatSnackBar } from '@angular/material';
 import * as Handsontable from 'handsontable';
 import { HotTableRegisterer } from '@handsontable-pro/angular';
 
+declare var tableau: any;
+
 @Component({
     templateUrl: 'add.component.html',
     styleUrls: ['table.component.scss'],
@@ -34,6 +36,7 @@ export class AddComponent implements OnInit, OnDestroy {
         private authService: AuthService,
         private snackBar: MatSnackBar,
     ) {
+        tableau.extensions.initializeAsync();
         this.columns = this.dbService.getCurrentTableDef();
         this.resetTableColumns(this.columns);
     }
@@ -44,6 +47,7 @@ export class AddComponent implements OnInit, OnDestroy {
     ngOnDestroy() {}
 
     resetTableColumns(columns) {
+        'use strict';
         const obj = {};
         this.dataset = [];
 
@@ -84,6 +88,7 @@ export class AddComponent implements OnInit, OnDestroy {
             }
             gg.push(row);
         });
+        let test = '';
 
         try {
             await this.dbService.addEntries({ rows: gg }).toPromise();
@@ -96,16 +101,14 @@ export class AddComponent implements OnInit, OnDestroy {
                 verticalPosition: 'top',
             });
             this.resetTableColumns(this.columns);
+            console.log(tableau.extensions, 'data');
+            const datasources = await tableau.extensions.dashboardContent.dashboard.worksheets[0].getDataSourcesAsync();
+            datasources[0].refreshAsync();
         } catch (error) {
-            console.log(error, 'error');
-            this.snackBar.open(
-                'An error occur, kindly check the record and try again',
-                'dismiss',
-                {
-                    duration: 5000,
-                    verticalPosition: 'top',
-                },
-            );
+            this.snackBar.open(error, 'dismiss', {
+                duration: 5000,
+                verticalPosition: 'top',
+            });
         }
     }
 
